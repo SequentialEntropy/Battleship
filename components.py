446a68f44372387dec7fc2572a309d22310cae7c
Skytec711 <asahi79289@gmail.com>
2023-12-08 17:45:24 +0000

    Add place_battleships with custom InvalidAlgorithmException
    Add custom exception InvalidAlgorithmException
    
    - Takes in one `algorithm` parameter, the name of the (non-existent) algorithm attempted to be used in `place_battleships`
    
    Add place_battleships method
    
    - Takes in the `board` parameter, a list of lists - as per coursework specs
    
    - Takes in the `ships` parameter, a dictionary with a `<string: int>` pair derived from `create_battleships` - as per coursework
    specs
    
    - Takes in the `algorithm` parameter, a string representing the type of placement algorithm with a default value of `simple` - as per
    coursework specs
    
    - At this point, the method always returns an error as no algorithms are implemented yet

--- components.py
+++ components.py
@@ -9,3 +9,17 @@ def create_battleships(filename="battleships.txt"):
         ships = {line.split(":")[0]: int(line.split(":")[1].replace("\n", "")) for line in lines}
 
         return ships
+
+
+
+# Custom exception for an invalid algorithm, where the algorithm is not defined as an option in place_battleships
+class InvalidAlgorithmException(Exception):
+    def __init__(self, algorithm):
+        self.message = f"The algorithm '{algorithm}' is not defined"
+    def __str__(self):
+        return self.message
+
+def place_battleships(board, ships, algorithm="simple"):
+    match algorithm:
+        case _:
+            raise InvalidAlgorithmException(algorithm)
