1f669970f6d3817d10538d04329949a3ee6dafd2
Skytec711 <asahi79289@gmail.com>
2023-12-10 23:31:33 +0000

    Track attack history to colour code and show or hide cells

--- mp_game_engine.py
+++ mp_game_engine.py
@@ -1,8 +1,12 @@
 import components
 import random
-from game_engine import attack, cli_coordinates_input, print_board
+from game_engine import attack, cli_coordinates_input
 
 players = {}
+
+red = "\033[91m"
+green = "\033[92m"
+reset = "\033[0m"
 
 # Pregenerate attack sequence to prevent duplicate attacks
 def attack_sequence(board_size):
@@ -14,32 +18,44 @@ def attack_sequence(board_size):
 def generate_attack(board_size=10):
     return next(attack_sequence(board_size))
 
+def print_board(board, board_history, show_ships=True):
+    board_length = len(board)
+
+    for y in range(board_length):
+        for x in range(board_length):
+
+            colour = red if (x, y) in board_history else reset
+            
+            shape = "##" if (board[y][x] is not None and show_ships) or board_history.get((x, y), False) else "::"
+
+            print(f"{colour}{shape}{reset}", end=" ")
+
+        print()
+
 def ai_opponent_game_loop():
     print("Welcome to Battleship")
 
     players["Player"] = {
         "board": components.place_battleships(components.initialise_board(), components.create_battleships(), "custom"),
         "battleships": components.create_battleships(),
+        "board_history": {} # Store the coords:result as (int, int):bool to track the AI's attack attempt history
     }
 
     players["AI"] = {
         "board": components.place_battleships(components.initialise_board(), components.create_battleships(), "random"),
-        "battleships": components.create_battleships()
+        "battleships": components.create_battleships(),
+        "board_history": {} # Store the coords:result as (int, int):bool to track the Player's attack attempt history
     }
 
     print("-" * 29)
 
-    red = "\033[91m"
-    green = "\033[92m"
-    reset = "\033[0m"
-
     while True:
         # Print Boards
         print("AI's board")
-        print_board(players["AI"]["board"])
+        print_board(players["AI"]["board"], players["AI"]["board_history"], show_ships=False)
 
         print("\nYour board")
-        print_board(players["Player"]["board"])
+        print_board(players["Player"]["board"], players["Player"]["board_history"], show_ships=True)
 
         print("-" * 29)
 
@@ -50,6 +66,9 @@ def ai_opponent_game_loop():
 
         hit_or_miss = attack(coordinates, players["AI"]["board"], players["AI"]["battleships"])
         print(f"You attacked {coordinates} - {green}Hit{reset}" if hit_or_miss else f"You attacked {coordinates} - {red}Miss{reset}")
+        players["AI"]["board_history"][coordinates] = hit_or_miss
+
+        print(players["AI"]["board_history"])
 
         # All AI ships are sunk
         if all(ship_length == 0 for ship_length in players["AI"]["battleships"].values()):
@@ -61,6 +80,9 @@ def ai_opponent_game_loop():
 
         hit_or_miss = attack(coordinates, players["Player"]["board"], players["Player"]["battleships"])
         print(f"AI  attacked {coordinates} - {red}Hit{reset}" if hit_or_miss else f"AI  attacked {coordinates} - {green}Miss{reset}")
+        players["Player"]["board_history"][coordinates] = hit_or_miss
+
+        print(players["Player"]["board_history"])
 
         # All Player ships are sunk
         if all(ship_length == 0 for ship_length in players["Player"]["battleships"].values()):
