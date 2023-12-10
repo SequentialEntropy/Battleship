7897b3e23d55b805ea61f7f4daa1949e7a837e7b
Skytec711 <asahi79289@gmail.com>
2023-12-10 17:02:33 +0000

    Implement cli_coordinates_input in game_engine

--- game_engine.py
+++ game_engine.py
@@ -10,3 +10,23 @@ def attack(coordinates, board, battleships):
     battleships[ship_name] -= 1
 
     return True
+
+def cli_coordinates_input():
+    
+    while True:
+        val = input("Enter the x-coordinate to attack: ")
+        try:
+            x = int(val)
+            break
+        except ValueError:
+            print(f"{val} is not a number")
+
+    while True:
+        val = input("Enter the y-coordinate to attack: ")
+        try:
+            y = int(val)
+            break
+        except ValueError:
+            print(f"{val} is not a number")
+
+    return (x, y)
