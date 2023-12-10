1890477e9fa053b8dede37be7772fd150fef6c7f
Skytec711 <asahi79289@gmail.com>
2023-12-10 21:51:31 +0000

    Implement ai_opponent_game_loop in mp_game_engine
    Player and AI ship coordinates are printed as-is and are not obfuscated
    
    Player and AI attempts are not tracked

--- mp_game_engine.py
+++ mp_game_engine.py
@@ -1,4 +1,8 @@
+import components
 import random
+from game_engine import attack, cli_coordinates_input, print_board
+
+players = {}
 
 # Pregenerate attack sequence to prevent duplicate attacks
 def attack_sequence(board_size):
@@ -9,3 +13,64 @@ def attack_sequence(board_size):
 
 def generate_attack(board_size=10):
     return next(attack_sequence(board_size))
+
+def ai_opponent_game_loop():
+    print("Welcome to Battleship")
+
+    players["Player"] = {
+        "board": components.place_battleships(components.initialise_board(), components.create_battleships(), "custom"),
+        "battleships": components.create_battleships(),
+    }
+
+    players["AI"] = {
+        "board": components.place_battleships(components.initialise_board(), components.create_battleships(), "random"),
+        "battleships": components.create_battleships()
+    }
+
+    print("-" * 29)
+
+    red = "\033[91m"
+    green = "\033[92m"
+    reset = "\033[0m"
+
+    while True:
+        # Print Boards
+        print("AI's board")
+        print_board(players["AI"]["board"])
+
+        print("\nYour board")
+        print_board(players["Player"]["board"])
+
+        print("-" * 29)
+
+        # Player attacks AI
+        coordinates = cli_coordinates_input()
+
+        print("-" * 29)
+
+        hit_or_miss = attack(coordinates, players["AI"]["board"], players["AI"]["battleships"])
+        print(f"You attacked {coordinates} - {green}Hit{reset}" if hit_or_miss else f"You attacked {coordinates} - {red}Miss{reset}")
+
+        # All AI ships are sunk
+        if all(ship_length == 0 for ship_length in players["AI"]["battleships"].values()):
+            print(f"{green}You Won{reset}")
+            break
+
+        # AI attacks Player
+        coordinates = generate_attack()
+
+        hit_or_miss = attack(coordinates, players["Player"]["board"], players["Player"]["battleships"])
+        print(f"AI  attacked {coordinates} - {red}Hit{reset}" if hit_or_miss else f"AI  attacked {coordinates} - {green}Miss{reset}")
+
+        # All Player ships are sunk
+        if all(ship_length == 0 for ship_length in players["Player"]["battleships"].values()):
+            print(f"{red}AI Won{reset}")
+            break
+
+        print("-" * 29)
+
+    # Game Over
+    print("Game Over")
+
+if __name__ == "__main__":
+    ai_opponent_game_loop()
