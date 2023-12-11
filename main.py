fff7be1774aa2e4cc2183704bfaeb178bcad5cce
Skytec711 <asahi79289@gmail.com>
2023-12-11 04:11:10 +0000

    Use players dict from mp_game_engine to store game data

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 from flask import Flask, request, render_template, jsonify
+from mp_game_engine import players
+
 import components
 
 app = Flask(__name__)
 
 ships = components.create_battleships()
 board_size = 10
-placement = {}
-player_board = components.initialise_board(board_size)
 
 @app.route("/placement", methods=["GET", "POST"])
 def placement_interface():
-    global placement
-
     if request.method == "GET":
         return render_template("placement.html", ships=ships, board_size=board_size)
     
     elif request.method == "POST":
         
         placement = request.json
+
+        players["Player"] = {
+            "board": components.place_battleships(components.initialise_board(), components.create_battleships(), "custom", placement),
+            "battleships": components.create_battleships(),
+            "board_history": {} # Store the coords:result as (int, int):bool to track the AI's attack attempt history
+        }
+
+        players["AI"] = {
+            "board": components.place_battleships(components.initialise_board(), components.create_battleships(), "random"),
+            "battleships": components.create_battleships(),
+            "board_history": {} # Store the coords:result as (int, int):bool to track the Player's attack attempt history
+        }
+
         return jsonify({"message": "Success!"})
 
 @app.route("/")
 def root():
-    # global placement
-    # return placement
+    player_board = players["Player"]["board"]
     return render_template("main.html", player_board=player_board)
 
 @app.route("/attack", methods=["GET"])
